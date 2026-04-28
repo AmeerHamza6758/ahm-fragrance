055a57173fa4829373b06a7d04e11cfc3199fe1f
Ameer Hamza <142672783+AmeerHamza6758@users.noreply.github.com>
2026-04-28 13:52:53 +0500

    update admin

---
+++ admin/src/features/OrdersPage.jsx
@@ -0,0 +1,151 @@
+import PageSection from "../components/PageSection";
+import "../styles/admin.css"
+
+function OrdersPage() {
+  return (
+    <div className="orders">
+
+      {/* Header */}
+      <div className="orders-header">
+        <h1 className="orders-title">Order Registry</h1>
+        <p className="orders-subtitle">
+          Managing the factory journey of our clients.
+        </p>
+      </div>
+
+      {/* Filters */}
+      <div className="orders-filters">
+
+        <input
+          type="text"
+          placeholder="Search orders, customers, or tracking IDs..."
+          className="orders-search"
+        />
+
+        <div className="filters-right">
+          <select className="filter-select">
+            <option>Status: All</option>
+          </select>
+
+          <select className="filter-select">
+            <option>Date: Last 30 Days</option>
+          </select>
+
+          <button className="filter-btn">⚙</button>
+        </div>
+      </div>
+
+      {/* Table */}
+      <div className="orders-table">
+
+        <div className="orders-table-header">
+          <span>Order ID</span>
+          <span>Date</span>
+          <span>Customer</span>
+          <span>Total (PKR)</span>
+          <span>Order Status</span>
+          <span>Payment</span>
+          <span>Actions</span>
+        </div>
+
+        {/* Row 1 */}
+        <div className="orders-row">
+          <span>#AHM-9284</span>
+          <span>Oct 24, 2023</span>
+          <span>Evelyn Thorne</span>
+          <span>24,500</span>
+          <span className="status-shipped">Shipped</span>
+          <span className="payment-paid">Paid</span>
+          <span>•••</span>
+        </div>
+
+        {/* Row 2 */}
+        <div className="orders-row">
+          <span>#AHM-9285</span>
+          <span>Oct 24, 2023</span>
+          <span>Julian Vane</span>
+          <span>18,200</span>
+          <span className="status-pending">Pending</span>
+          <span className="payment-pending">Pending</span>
+          <span>•••</span>
+        </div>
+
+        {/* Expanded Detail */}
+        <div className="order-details">
+
+          {/* Left */}
+          <div className="order-composition">
+            <h3>Order Composition</h3>
+
+            <div className="composition-item">
+              <div style={{marginTop:"10px", height:"70px"}}>
+                <h4>Oud Royale</h4>
+                <p>100ML Selection</p>
+              </div>
+              <span style={{marginTop:"60px"}}>PKR 12,500</span>
+            </div>
+
+            <div className="composition-item">
+              <div style={{paddingBottom:"10px", height:"70px"}}>
+                <h4 style={{}}>Velvet Peony</h4>
+                <p>50ML Selection</p>
+              </div>
+              <span style={{marginTop:"60px"}}>PKR 5,700</span>
+            </div>
+          </div>
+
+          {/* Right */}
+          <div className="customer-info">
+            <h3 style={{paddingTop:"20px"}}>Customer Information</h3>
+
+            <p><strong>Full Name:</strong> Julian Vane</p>
+            <p><strong>Phone:</strong> 0304-XXXXXXX</p>
+            <p><strong>Email:</strong> j.vane@editorial.com</p>
+            <p><strong>Address:</strong>Suite 402, Sterling heights, Phase 6, DHA, Karachi, Pakistan</p>
+            <p><strong>Postal Code:</strong> 75500</p>
+            <p><strong>Delivery Charges:</strong> PKR 500</p>
+          </div>
+
+        </div>
+        <div className="orders-row">
+         <span>#AHM-9286</span>
+         <span>Oct 23, 2023</span>
+         <span>Clara Beaumont</span>
+         <span>36,800</span>
+         <span className="status-confirmed">Confirmed</span>
+         <span className="payment-paid">Paid</span>
+         <span>•••</span>
+        </div>
+        <div className="orders-row">
+         <span>#AHM 9287</span>
+         <span>Oct 23, 2023</span>
+         <span>Marcus Chen</span>
+         <span>12,250</span>
+         <span className="status-deliver">Delivered</span>
+         <span className="payment">Paid</span>
+         <span>•••</span>
+        </div>
+        <div className="orders-row">
+         <span>#AHM-9288</span>
+         <span>Oct 22, 2023</span>
+         <span>Aria Sterling</span>
+         <span>9,500</span>
+         <span className="status-can">Cancelled</span>
+         <span className="payment-fail">Failed</span>
+         <span>•••</span>
+        </div>
+        <div className="orders-row">
+         <span>#AHM-9289</span>
+         <span>Oct 21, 2023</span>
+         <span>Zephyr Khan</span>
+         <span>15,000</span>
+         <span className="status-refund">Refunded</span>
+         <span className="payment-refund">Refunded</span>
+         <span>•••</span>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+export default OrdersPage;
